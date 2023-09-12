2a32d1d6ccb510166c2ac30c2920f83e5542957f
Brian Schlining <bschlining@gmail.com>
2023-09-12 13:06:09 -0700

    Unit tests updated for latest API changes (#20)

--- test/test_darwincore.py
+++ test/test_darwincore.py
@@ -4,9 +4,6 @@ from fathomnet.api import darwincore
 
 
 class TestDarwinCoreAPI(TestCase):
-    def test_index(self):
-        index = darwincore.index()
-        self.assertIsNotNone(index)
 
     def test_find_owner_institution_codes(self):
         owner_institution_codes = darwincore.find_owner_institution_codes()
